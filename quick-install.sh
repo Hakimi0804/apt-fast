456413ea1e7e337fb40e7a2e7376ac082b976a04
Hakimi0804 <hakimifirdaus944@gmail.com>
2022-04-17 06:41:22 +0800

    installer: Make compatible with termux
    Signed-off-by: Hakimi0804 <hakimifirdaus944@gmail.com>

--- quick-install.sh
+++ quick-install.sh
@@ -7,13 +7,30 @@ apt_fast_installation() {
     sudo apt-get install -y aria2
   fi
 
-  wget https://raw.githubusercontent.com/ilikenwf/apt-fast/master/apt-fast -O /usr/local/sbin/apt-fast
-  chmod +x /usr/local/sbin/apt-fast
-  if ! [[ -f /etc/apt-fast.conf ]]; then
-    wget https://raw.githubusercontent.com/ilikenwf/apt-fast/master/apt-fast.conf -O /etc/apt-fast.conf
+  wget https://raw.githubusercontent.com/Hakimi0804/apt-fast/master/apt-fast -O "${PREFIX%/usr}/usr/local/bin/apt-fast"
+  chmod +x "${PREFIX%/usr}/usr/local/bin/apt-fast"
+  if ! [[ -f "${PREFIX}/etc/apt-fast.conf" ]]; then
+    wget https://raw.githubusercontent.com/Hakimi0804/apt-fast/master/apt-fast.conf -O "${PREFIX}/etc/apt-fast.conf"
   fi
 }
 
+
+if [[ -n "$PREFIX" ]]; then
+  # We're in termux
+
+  remove=false
+  [ ! -d "${PREFIX%/usr}"/usr/local/bin ] && { mkdir -p "${PREFIX%/usr}/usr/local/bin"; remove=true; }
+  apt_fast_installation
+  mv "${PREFIX%/usr}/usr/local/bin/apt-fast" "${PREFIX}/bin"
+
+  if [[ "$remove" = true ]]; then
+  	rm -rf "${PREFIX%/usr}/usr/local/bin"
+  fi
+
+  termux-fix-shebang "$(command -v apt-fast)"
+
+  exit 0
+fi
 
 if [[ "$EUID" -eq 0 ]]; then
   apt_fast_installation
